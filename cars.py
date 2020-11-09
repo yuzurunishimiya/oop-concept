db089fb1f1c36d113254785bfc79e22b5e0ff7ea
Sony Alamsyah Megadiana <sonyalamsyahmegadiana92@gmail.com>
2020-11-09 23:02:58 +0700

    create class named Car

---
+++ cars.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, company, speed, color, year):
+        self.company = company
+        self.color = color
+        self.speed = speed
+        self.year = year
+
+    def get_speed(self):
+        print(self.speed)
+    
+    def get_color(self):
+        print(self.color)
+    
+    def get_company(self):
+        print(self.company)
+    
+    def get_year(self):
+        print(self.year)
+    
