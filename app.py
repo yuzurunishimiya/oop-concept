5ede8011022565e7128ee294fee8e910dac3bff6
Sony Alamsyah Megadiana <sonyalamsyahmegadiana92@gmail.com>
2020-11-09 23:03:11 +0700

    main app

---
+++ app.py
@@ -0,0 +1,9 @@
+from cars import Car
+
+
+if __name__ == "__main__":
+    car = Car("Honda", 200, "yellow", "2020")
+    car.get_color()
+    car.get_company()
+    car.get_speed()
+    car.get_year()
